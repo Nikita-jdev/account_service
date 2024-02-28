7aa19dda6856a8a9c450aeffc3e48a17159037e1
Fedor Shtanko <atraxlxxxl@gmail.com>
2024-02-29 02:48:54 +0300

    создана таблица

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,1 +1,14 @@
--- Write your sql migration here!
+CREATE TABLE IF NOT EXISTS request
+(
+    token          UUID PRIMARY KEY DEFAULT,
+    user_id        BIGINT NOT NULL UNIQUE,
+    request_type   varchar(60) NOT NULL,
+    lock_value     BIGINT NOT NULL,
+    is_open        boolean default true,
+    request_data   jsonb NOT NULL,
+    request_status varchar(60) NOT NULL,
+    addit_status   varchar(130),
+    created_at     timestamptz DEFAULT current_timestamp,
+    updated_at     timestamptz DEFAULT current_timestamp,
+    version        smallint default 1
+);
