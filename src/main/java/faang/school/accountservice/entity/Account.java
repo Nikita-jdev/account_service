78645613ebe761475cd0513a09a97840f60f2607
Rikaf <fakir207@mail.ru>
2024-03-11 00:44:26 +0500

    sleeping

---
+++ src/main/java/faang/school/accountservice/entity/Account.java
@@ -0,0 +1,63 @@
+package faang.school.accountservice.entity;
+
+import faang.school.accountservice.enums.AccountStatus;
+import faang.school.accountservice.enums.Currency;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "account")
+public class Account {
+
+    @jakarta.persistence.Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(min = 12, max = 20)
+    @Pattern(regexp = "\\d{12,20}")
+    @Column(name = "number", nullable = false, unique = true)
+    private String number;
+
+    @Column(name = "owner_id", nullable = false)
+    private Long ownerId;
+
+    @Column(name = "currency")
+    @Enumerated(EnumType.STRING)
+    private Currency currency;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private AccountStatus status;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "closed_at")
+    private LocalDateTime closedAt;
+
+    @Version()
+    @Column(name = "version", nullable = false)
+    private Long version;
+
+}
---
+++ src/main/java/faang/school/accountservice/enums/AccountStatus.java
@@ -0,0 +1,8 @@
+package faang.school.accountservice.enums;
+
+public enum AccountStatus {
+    ACTIVE,
+    FROZEN,
+    CLOSED
+
+}
