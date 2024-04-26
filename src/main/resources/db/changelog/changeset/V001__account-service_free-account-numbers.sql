783cb8cd89a34b88a6401be96652c4b62641b7e6
DadyaVolodya <pl4stmass@yandex.ru>
2024-04-26 21:04:27 +0300

    do migration

--- src/main/resources/db/changelog/changeset/V001__account-service_free-account-numbers.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_free-account-numbers.sql
@@ -2,5 +2,5 @@ CREATE TABLE free_account_numbers (
   id SERIAL PRIMARY KEY,
   account_type VARCHAR(255) NOT NULL,
   account_number VARCHAR(20) NOT NULL,
-  UNIQUE INDEX idx_free_account_numbers_unique (account_type, account_number)
+  unique (account_type, account_number)
 );
