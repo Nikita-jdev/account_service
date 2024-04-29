fb32625eb8704f5832f91c44fdb2f23ebe838b6a
DadyaVolodya <pl4stmass@yandex.ru>
2024-04-29 14:46:36 +0300

    коммит

--- src/main/java/faang/school/accountservice/service/FreeAccountNumbersService.java
+++ src/main/java/faang/school/accountservice/service/FreeAccountNumbersService.java
@@ -6,19 +6,16 @@ public class FreeAccountNumbersService {
     private final AccountNumbersSequenceRepository accountNumbersSequenceRepository;
 
     public String generateNewAccountNumber(String accountType) {
-        // Попытка получить и удалить первый свободный номер
         FreeAccountNumber freeAccountNumber = freeAccountNumberRepository.getAndDeleteFirstFreeAccountNumber();
 
         if (freeAccountNumber != null) {
             return freeAccountNumber.getAccountNumber();
         }
-
-        // Свободного номера нет, инкрементируем счетчик и генерируем новый
+        
         accountNumbersSequenceRepository.incrementSequenceIfValueMatches(accountType, 0); // Инкремент с 0
         long nextNumber = accountNumbersSequenceRepository.getCurrentNumber(accountType); // Получить текущее значение счетчика
         String accountNumber = accountType + String.format("%012d", nextNumber); // Форматирование номера
-
-        // Сохранить новый номер в free_account_numbers
+        
         freeAccountNumberRepository.create(nextNumber, accountType);
 
         return accountNumber;
