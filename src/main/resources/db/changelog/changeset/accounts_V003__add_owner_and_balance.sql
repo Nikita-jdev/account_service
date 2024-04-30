dbf898d1726afb4a25debae293323eea92b67667
Dmitriy Goncharov <incoom@yandex.ru>
2024-05-01 04:15:20 +0500

    sql...

--- src/main/resources/db/changelog/changeset/accounts_V003__add_owner_and_balance.sql
+++ src/main/resources/db/changelog/changeset/accounts_V003__add_owner_and_balance.sql
@@ -1,9 +1,8 @@
 CREATE TABLE owner (
     id              BIGINT      PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
     account_id      BIGINT      NOT NULL,
-    owner_type      VARCHAR(24) NOT NULL;
-)
+    owner_type      VARCHAR(24) NOT NULL
+);
 
-ALTER TABLE account (
-    ADD COLUMN balance_id   BIGINT  NOT NULL;
-)
+ALTER TABLE account
+ADD COLUMN balance_id   BIGINT  NOT NULL;
