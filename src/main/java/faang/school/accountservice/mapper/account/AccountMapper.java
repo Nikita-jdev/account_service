d11010a51ec2210c7feb7d4c41167a408e8775ae
Dmitriy Goncharov <incoom@yandex.ru>
2024-05-01 04:09:19 +0500

    added tests

--- src/main/java/faang/school/accountservice/mapper/account/AccountMapper.java
+++ src/main/java/faang/school/accountservice/mapper/account/AccountMapper.java
@@ -3,6 +3,7 @@ package faang.school.accountservice.mapper.account;
 import faang.school.accountservice.dto.account.AccountDto;
 import faang.school.accountservice.model.account.Account;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
@@ -10,8 +11,10 @@ import java.util.List;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AccountMapper {
 
+    @Mapping(source = "owner.id", target = "ownerId")
     AccountDto toDto(Account account);
 
+    @Mapping(source = "ownerId", target = "owner.id")
     Account toEntity(AccountDto accountDto);
 
     List<AccountDto> toDto(List<Account> accounts);
--- src/test/java/faang/school/accountservice/service/AccountServiceTest.java
+++ src/test/java/faang/school/accountservice/service/AccountServiceTest.java
@@ -6,6 +6,7 @@ import faang.school.accountservice.mapper.account.AccountMapperImpl;
 import faang.school.accountservice.model.account.Account;
 import faang.school.accountservice.model.account.AccountStatus;
 import faang.school.accountservice.model.account.AccountType;
+import faang.school.accountservice.model.owner.Owner;
 import faang.school.accountservice.repository.AccountRepository;
 import faang.school.accountservice.validation.AccountValidator;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,7 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -45,14 +47,14 @@ class AccountServiceTest {
         account = Account.builder()
                 .id(1L)
                 .number("1234567890123")
-                .ownerId(10L)
+                .owner(Owner.builder().id(10L).build())
                 .accountType(AccountType.INDIVIDUAL)
                 .currency(Currency.USD)
                 .build();
         accountDto = AccountDto.builder()
                 .id(account.getId())
                 .number(account.getNumber())
-                .ownerId(account.getOwnerId())
+                .ownerId(account.getOwner().getId())
                 .accountType(account.getAccountType())
                 .currency(account.getCurrency())
                 .build();
@@ -78,5 +80,48 @@ class AccountServiceTest {
         when(accountRepository.findById(account.getId())).thenReturn(Optional.ofNullable(account));
 
         AccountDto returned = accountService.get(10L, 1L);
+
+        assertAll(
+                () -> verify(accountRepository, times(1)).findById(1L),
+                () -> verify(accountMapper, times(1)).toDto(account),
+                () -> assertEquals(accountDto, returned)
+        );
+    }
+
+    @Test
+    void getAllOwnerAccounts_AccountsFoundThenReturnedAsDto() {
+        when(accountRepository.findByOwnerId(10L)).thenReturn(List.of(account));
+
+        List<AccountDto> returned = accountService.getAllOwnerAccounts(10L);
+
+        assertAll(
+                () -> verify(accountRepository, times(1)).findByOwnerId(10L),
+                () -> verify(accountMapper, times(1)).toDto(List.of(account)),
+                () -> assertEquals(List.of(accountDto), returned)
+        );
+    }
+
+    @Test
+    void block_AccountIsBlocked_Valid() {
+        when(accountRepository.findById(account.getId())).thenReturn(Optional.ofNullable(account));
+
+        accountService.block(10L, 1L);
+
+        assertAll(
+                () -> verify(accountRepository, times(1)).findById(1L),
+                () -> assertEquals(AccountStatus.INACTIVE, account.getAccountStatus())
+        );
+    }
+
+    @Test
+    void close_AccountIsClosed_Valid() {
+        when(accountRepository.findById(account.getId())).thenReturn(Optional.ofNullable(account));
+
+        accountService.close(10L, 1L);
+
+        assertAll(
+                () -> verify(accountRepository, times(1)).findById(1L),
+                () -> assertEquals(AccountStatus.CLOSED, account.getAccountStatus())
+        );
     }
 }
