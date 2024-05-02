36d57d8a555e8df00ed71d033e5ac45014cef9ca
DadyaVolodya <pl4stmass@yandex.ru>
2024-05-02 23:42:47 +0300

    Start developing

--- src/main/resources/db/changelog/changeset/V001__account-service_free-account-numbers.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_free-account-numbers.sql
@@ -1,10 +1,11 @@
-create TABLE free_account_numbers (
-  id bigint primary key GENERATED ALWAYS AS IDENTITY unique;
-  account_type VARCHAR(255) NOT NULL,unique
-  account_number VARCHAR(20) NOT NULL,unique
+CREATE TABLE free_account_numbers (
+  id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  account_type VARCHAR(255) NOT NULL,
+  account_number VARCHAR(20) NOT NULL,
+  UNIQUE (account_type, account_number)
 );
-create TABLE account_numbers_sequence (
-  id bigint primary key GENERATED ALWAYS AS IDENTITY unique;
-  account_type VARCHAR(255) NOT NULL,unique
+
+CREATE TABLE account_numbers_sequence (
+  account_type VARCHAR(255) PRIMARY KEY,
   current_number BIGINT NOT NULL DEFAULT 0
 );
