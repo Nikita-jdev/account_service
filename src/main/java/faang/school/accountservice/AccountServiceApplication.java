3ca4a56a2d49d87b0a25974cff58fb0dd3630df5
DadyaVolodya <pl4stmass@yandex.ru>
2024-05-11 11:56:57 +0300

    cross review

--- src/main/java/faang/school/accountservice/AccountServiceApplication.java
+++ src/main/java/faang/school/accountservice/AccountServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -12,7 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients("faang.school.accountservice.client")
-@EnableJpaRepositories
 public class AccountServiceApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(AccountServiceApplication.class)
--- src/main/java/faang/school/accountservice/model/account/numbers/AccountNumberSequence.java
+++ src/main/java/faang/school/accountservice/model/account/numbers/AccountNumberSequence.java
@@ -6,13 +6,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
-@NoArgsConstructor
-@Getter
-@Setter
+@Data
 @Entity
 @Table(name = "account_numbers_sequence")
 public class AccountNumberSequence {
