e819309bb891388bfe0d639a0d6efd52a1382e8a
DadyaVolodya <pl4stmass@yandex.ru>
2024-05-21 20:44:06 +0300

    cross review

--- src/main/java/faang/school/accountservice/controller/cashback/CashbackTariffController.java
+++ src/main/java/faang/school/accountservice/controller/cashback/CashbackTariffController.java
@@ -3,6 +3,7 @@ package faang.school.accountservice.controller.cashback;
 import faang.school.accountservice.model.cashback.CashbackOperationMapping;
 import faang.school.accountservice.model.cashback.CashbackTariff;
 import faang.school.accountservice.service.cashback.CashbackTariffService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,14 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/cashback-tariffs")
 public class CashbackTariffController {
 
     private final CashbackTariffService cashbackTariffService;
-
-    public CashbackTariffController(CashbackTariffService cashbackTariffService) {
-        this.cashbackTariffService = cashbackTariffService;
-    }
 
     @PostMapping
     public CashbackTariff createTariff(@RequestBody CashbackTariff cashbackTariff) {
