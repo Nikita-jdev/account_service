1bf44eabdedf93a03db2044e4c1a51ce2ee2a933
DadyaVolodya <pl4stmass@yandex.ru>
2024-05-21 19:58:58 +0300

    cross review

--- src/main/resources/db/changelog/changeset/accounts_V005__add_free-account-numbers.sql
+++ src/main/resources/db/changelog/changeset/accounts_V005__add_free-account-numbers.sql
@@ -3,6 +3,7 @@ CREATE TABLE free_account_numbers (
   account_number BIGINT NOT NULL,
   CONSTRAINT free_pr_key PRIMARY KEY (type, account_number)
 );
+
 CREATE TABLE account_numbers_sequence (
   type VARCHAR(32) NOT NULL PRIMARY KEY,
   counter BIGINT NOT NULL DEFAULT 1
